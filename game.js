74e6580f94b9c56d641d511011c4d9f6ad959109
George Montgomery <george.p.montgomery@gmail.com>
2015-03-20 18:47:31 -0400

    Added color to the coin

--- game.js
+++ game.js
@@ -724,6 +724,8 @@ Coin = function () {
               0, -5]);
 
   this.collidesWith = ["ship"];
+  this.solid = true;
+  this.color = 'gold';
 
   this.newPosition = function () {
     this.x = Math.random() * Game.canvasWidth;
